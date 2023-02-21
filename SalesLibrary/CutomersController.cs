6a714051d75923177687409ccb78ed3abca25a43
Skyler Adams <skyler14adams@gmail.com>
2023-02-21 15:08:19 -0500

    Add project files.

---
+++ SalesLibrary/CutomersController.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesLibrary;
+
+public class CustomersController
+{
+    private SqlConnection sqlConnection { get; set; }
+
+    public List<Customer> GetBySales(decimal low, decimal high)
+    {
+        var sql = "SELECT * from Customers " +
+                $" Where Sales >= {low} and Sales <= {high}; ";
+        var cmd = new SqlCommand(sql, sqlConnection);
+        SqlDataReader reader = cmd.ExecuteReader();
+        List<Customer> customers = new List<Customer>();
+        while (reader.Read())
+        {
+            Customer customer = new Customer();
+            customer.Id = Convert.ToInt32(reader["ID"]);
+            customer.Name = Convert.ToString(reader["Name"])!;
+            customer.City = Convert.ToString(reader["City"])!;
+            customer.State = Convert.ToString(reader["State"])!;
+            customer.Sales = Convert.ToDecimal(reader["Sales"]);
+            customer.Active = Convert.ToBoolean(reader["active"]);
+            customers.Add(customer);
+        }
+        reader.Close();
+        return customers;
+    }
+
+    public List<Customer> GetAll()
+    {
+        var sql = "SELECT * from Customers;";
+        var cmd = new SqlCommand(sql, sqlConnection);
+        SqlDataReader reader = cmd.ExecuteReader();
+        List<Customer> customers = new List<Customer>();
+        while(reader.Read())
+        {
+            Customer customer = new Customer();
+            customer.Id = Convert.ToInt32(reader["ID"]);
+            customer.Name = Convert.ToString(reader["Name"])!;
+            customer.City = Convert.ToString(reader["City"])!;
+            customer.State = Convert.ToString(reader["State"])!;
+            customer.Sales = Convert.ToDecimal(reader["Sales"]);
+            customer.Active = Convert.ToBoolean(reader["active"]);
+            customers.Add(customer);
+        }
+        reader.Close();
+        return customers;
+    }
+
+    public Customer? GetById(int Id)
+    {
+        var sql = $"SELECT * from Customers Where Id = {Id};";
+        var cmd = new SqlCommand(sql, sqlConnection);
+        SqlDataReader reader = cmd.ExecuteReader();
+        if (!reader.HasRows)
+        {
+            reader.Close();
+            return null;
+        }
+        reader.Read();
+        Customer customer = new Customer();
+        customer.Id = Convert.ToInt32(reader["ID"]);
+        customer.Name = Convert.ToString(reader["Name"])!;
+        customer.City = Convert.ToString(reader["City"])!;
+        customer.State = Convert.ToString(reader["State"])!;
+        customer.Sales = Convert.ToDecimal(reader["Sales"]);
+        customer.Active = Convert.ToBoolean(reader["active"]);
+        reader.Close();
+        return customer;
+    }
+
+    public bool Add(Customer cust)
+    {
+        var sql = "INSERT Customers (Name, city, State, Sales, Active) VALUES " +
+                    $" ('{cust.Name}','{cust.City}','{cust.State}', {cust.Sales}, {(cust.Active ? 1 : 0)});";
+        var cmd = new SqlCommand(sql, sqlConnection);
+        var rowsAffected = cmd.ExecuteNonQuery();
+        if(rowsAffected == 0)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public bool Update(Customer cust)
+    {
+        var sql = " UPDATE Customers SET " +
+                   $" Name = '{cust.Name}', " +
+                   $"City = '{cust.City}', " +
+                   $"State = '{cust.State}', " +
+                   $"Sales = {cust.Sales}, " +
+                   $"Active = {(cust.Active ? 1 : 0)} " +
+                   $"Where Id = {cust.Id}; ";
+         var cmd = new SqlCommand(sql, sqlConnection);
+         var rowsAffected = cmd.ExecuteNonQuery();
+         if (rowsAffected == 0)
+         {
+                return false;
+         }
+            return true;
+        
+    }
+
+    public bool Delete(int Id)
+    {
+        var sql = " DELETE Customers " +
+                   $"Where Id = {Id}; ";
+        var cmd = new SqlCommand(sql, sqlConnection);
+        var rowsAffected = cmd.ExecuteNonQuery();
+        if (rowsAffected == 0)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public CustomersController (string server, string instance)
+    {
+        var connStr = $"server={server}\\{instance};" +
+                        "database = Salesdb;" +
+                        "trusted_Connection=True;" +
+                        "trustservercertificate=true;";
+        sqlConnection = new SqlConnection(connStr);
+        sqlConnection.Open();
+        if(sqlConnection.State!= ConnectionState.Open)
+        {
+            throw new Exception("Connection Failed!");
+        }
+
+    }
+
+    public void CloseConnection()
+    {
+        if(sqlConnection.State == ConnectionState.Open)
+        {
+            sqlConnection.Close();
+        }
+    }
+}
+
---
+++ TestSalesLibrary/Program.cs
@@ -0,0 +1,65 @@
+﻿using SalesLibrary;
+
+CustomersController custCtrl= new ("localhost", "sqlexpress");
+
+var customers = custCtrl.GetBySales(20000, 50000);
+
+foreach(var cust in customers)
+{
+    Console.WriteLine($"{cust.Id} | {cust.Name} | {cust.City}, {cust.State} | {cust.Sales} | {cust.Active}");
+}
+
+//var max = custCtrl.GetById(36);
+
+//if (max == null)
+//{
+//    Console.WriteLine("Not Found");
+//    return;
+//}
+
+//max.Name = "MAX Technical training";
+
+//var success = custCtrl.Update(max);
+
+//if (!success)
+//{
+//    Console.WriteLine("Update Failed");
+//    return;
+//}
+
+//var newCustomer = new Customer
+//{
+//    Id = 0, Name = "MAX", City = "Mason", State = "OH", Sales = 1000, Active = true
+//};
+//var success = custCtrl.Add(newCustomer);
+//if(!success)
+//{
+//    Console.WriteLine("Add failed");
+//}
+
+
+//var customers = custCtrl.GetAll();
+
+//foreach(var customer in customers)
+//{
+//    Console.WriteLine($"The customer name is {customer.Name} Very Cool!");
+//    //var intro = "The customer name is ";
+//    //var outro = "Very cool!";
+//    //Console.WriteLine(intro + customer.Name + outro);
+//}
+
+
+//var success = custCtrl.Delete(36);
+
+//var cust = custCtrl.GetById(36);
+//if (cust is null)
+//{
+//    Console.WriteLine("Not Found!!");
+//}
+//else
+//{
+//    Console.WriteLine($"{cust.Id} | {cust.Name} | {cust.City}, {cust.State} | {cust.Sales} | {cust.Active}");
+//}
+
+
+custCtrl.CloseConnection();
